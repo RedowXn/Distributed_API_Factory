44500eedf62bb337ba6f69356053d5c436f70cc9
Redowan Ahmed Siyam <redowanahmed.com@gmail.com>
2025-06-29 01:23:23 +0600

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,5 @@
+// routes/api.php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\UserController;
+
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'show']);
